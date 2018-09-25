12ca06ac84493e6102cf3c7ed77e1a51aa2453b5
easin <1375343893@qq.com>
2018-09-25 15:51:09 +0800

    input from keyboard

---
+++ input.py
@@ -0,0 +1,11 @@
+name = input("Name:")
+age = input("Age:")
+score = input("Score:")
+msg = '''
+---------------------------info------------------
+name:%s
+age:%s
+score:%s
+----------------------------end------------------
+''' % (name, age, score)
+print(msg)
